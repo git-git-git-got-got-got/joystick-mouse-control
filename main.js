b5b7f2960117351ced7130a1751ada20b5e81757
Cody Fields <contact@leeous.com>
2020-08-12 08:30:07 -0400

    Begin experimenting with different workflow designs

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow () {
     // Create the browser window.
     const win = new BrowserWindow({
         width: 500,
-        height: 500,
+        height: 250,
         autoHideMenuBar: true,
         resizable: false,
         webPreferences: {
@@ -19,6 +19,21 @@ function createWindow () {
     // and load the index.html of the app.
     win.loadFile('index.html');
     win.webContents.openDevTools();
+}
+
+function showSettings () {
+    // Create the browser window.
+    const settingsWin = new BrowserWindow({
+        width: 500,
+        height: 250,
+        autoHideMenuBar: true,
+        resizable: false,
+        modal: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    win.loadFile('settings.html');
 }
 
 app.whenReady().then(createWindow).then(() => {
@@ -41,4 +56,8 @@ console.log("Looking for connected controllers...");
 
 ipcMain.on("clientMsg", (e, string) => {
     console.log(string);
+});
+
+ipcMain.on("openSettings", (e) => {
+    settingsWin.show()
 });
